2e154fd721a4bc6c3808080446ac58dbd608d138
girijapathi1994 <girijapathi1994@gmail.com>
2018-10-30 17:36:57 -0500

    Removed firebase properties

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase:{
-    apiKey: "AIzaSyC1niSNIoVz0LDGUyxe05j_wO4iyrce3-w",
-    authDomain: "ucm-blog.firebaseapp.com",
-    databaseURL: "https://ucm-blog.firebaseio.com",
-    projectId: "ucm-blog",
-    storageBucket: "ucm-blog.appspot.com",
-    messagingSenderId: "664211108079"
-  }
+  
 };
 
 /*
